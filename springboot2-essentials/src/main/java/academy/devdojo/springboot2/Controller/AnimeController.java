bef2858d85de7be89af492fe81764620d72081ed
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-12-28 08:00:53 -0300

    get by id

--- springboot2-essentials/src/main/java/academy/devdojo/springboot2/Controller/AnimeController.java
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2/Controller/AnimeController.java
@@ -8,6 +8,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,10 @@ public class AnimeController {
     public ResponseEntity <List<Anime>> list(){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return new ResponseEntity<>( animeService.listAll(), HttpStatus.OK);
+    }
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id){
+        return ResponseEntity.ok(animeService.findById(id));
     }
 
 }
