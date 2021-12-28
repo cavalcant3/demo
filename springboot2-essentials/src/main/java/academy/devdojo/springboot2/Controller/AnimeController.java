85fb098c407129555a7ca5fc5ae101c08f5e7811
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-12-28 08:19:01 -0300

    save method

--- springboot2-essentials/src/main/java/academy/devdojo/springboot2/Controller/AnimeController.java
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2/Controller/AnimeController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,14 +19,21 @@ import java.util.List;
 public class AnimeController {
     private final DateUtil dateUtil;
     private final AnimeService animeService;
+
     @GetMapping
-    public ResponseEntity <List<Anime>> list(){
+    public ResponseEntity<List<Anime>> list() {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return new ResponseEntity<>( animeService.listAll(), HttpStatus.OK);
+        return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
     }
+
     @GetMapping(path = "/{id}")
-    public ResponseEntity<Anime> findById(@PathVariable long id){
+    public ResponseEntity<Anime> findById(@PathVariable long id) {
         return ResponseEntity.ok(animeService.findById(id));
     }
 
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime) {
+        return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
+    }
 }
+
