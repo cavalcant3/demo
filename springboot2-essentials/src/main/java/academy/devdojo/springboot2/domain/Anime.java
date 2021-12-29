11df1cc78aad50dea28e96f56eb880d65111fd9c
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-12-29 17:16:55 -0300

    conectando com o postgres

--- springboot2-essentials/src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -2,9 +2,11 @@ package academy.devdojo.springboot2.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.hibernate.annotations.Table;
 
 @Data
 @AllArgsConstructor
+
 public class Anime {
     private Long id;
     private String name;
