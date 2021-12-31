9f34630f53977ed045ed53777adc4276060e10cc
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-12-30 22:57:39 -0300

    validação de campos

--- springboot2-essentials/src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -2,7 +2,12 @@ package academy.devdojo.springboot2.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class AnimePostRequestBody {
+    @NotNull(message = "The annime cannot be empty")
+    @NotEmpty(message = "the anime cannot be null")
     private String name;
 }
